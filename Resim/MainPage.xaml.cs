95e7d4cf8f5ec2adaa310c6c6ab2af37c1d82a4d
mpadir <muhammedpadir@gmail.com>
2019-02-16 15:18:24 +0300

    raspberry pi C# resim geçişi

---
+++ Resim/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// Boş Sayfa öğe şablonu https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x41f adresinde açıklanmaktadır
+
+namespace Resim
+{
+    /// <summary>
+    /// Kendi başına kullanılabilecek ya da bir Çerçeve içine taşınabilecek boş bir sayfa.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        String x = "C:\\Users\\mami\\Desktop\\resim\\website.jpg";
+         
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        
+
+        private void Click_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Res.Source = new BitmapImage(new Uri("ms-appx:///Assets/jpg.jpg"));
+           
+           
+
+
+
+        }
+    }
+}
